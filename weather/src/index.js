0577c570b5f8e67da7984ff26bd4bc7cb1adabeb
PG Work <paugoldb@amazon.com>
2019-10-16 12:13:31 -0700

    big up

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,71 +1,60 @@
+/*jshint esversion: 6 */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Request from 'superagent';
+import _ from 'lodash';
 
 
 class Input extends React.Component {
 
-    constructor(props) {
-        super(props);
-    
+    constructor() { 
+        super();
         this.state = {
-            input_field: '',
-            items: [],
-            isLoaded: false
+            zip: 98119
         };
     }
 
-    componentDidMount () {
-
-        fetch('http://api.openweathermap.org/data/2.5/weather?zip=98119&appid=65036b8d41536ad067f7d3079698ebcc')
-            .then(res => res.json())
-            .then(json => {
-                console.log(json)
-                this.setState({
-                    isLoaded: true,
-                    items: json.weather,
-                })
-            });
+    componentWillMount () {
+        this.search();
     }
 
-    update(event) {
-        this.setState({
+    updateSearch () {
+        this.search(this.refs.query.value);
+    }
 
-        })
+    clicked () {
+        this.setState({
+            zip: this.refs.query.value
+        });
+        this.search(this.refs.query.value);
     }
 
     render() {
-
-        var {isLoaded, items} = this.state;
-
-        if (!isLoaded) {
-            return (
-            <div>Loading...</div>
-            )
-        } else {
-            return React.createElement('ul', {}, items.map(item => 
-                React.createElement('li')))
-            return (
-                <ul>
-                    {items.map(item => (
-                        <li key={item.id}>
-                            {item.main}
-                        </li>
-                    ))}
-                </ul>
-            )
-        }
-
-        /* return(
-            <div>
-                <h4>It's weather time!</h4>
-                <input type='text'
-                value= {this.state.item_input}
-                onChange = {this.update} />
-                <button type = 'button'
-                onClick={this.add}>Search</button>
+        var weather = _.map(this.state.weather, (json) => {
+            //var temperature = {json.main.temp};
+            //var far = ((temperature − 273.15) × 9/5 + 32);
+            return <div>
+            <li>{ json.name }</li>
+            <li>{ json.temp }</li>
             </div>
-        ) */
+        });
+        return <div>
+            <input ref="query" type="text" />
+            <button onClick={ (e) => {this.clicked(); }}>Search</button>
+            <ul>{ weather }</ul>
+        </div>
+    }
+
+    search(query = "98119") {
+        var url = `http://api.openweathermap.org/data/2.5/weather?zip=${query}&appid=65036b8d41536ad067f7d3079698ebcc`;
+        Request.get(url).then((response) => {
+            console.log(response.body.name);
+            this.setState({
+                weather: response.body
+            });
+        });
+        
     }
 }
 
